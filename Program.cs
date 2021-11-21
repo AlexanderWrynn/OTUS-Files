9dd6e0a0761d3d854f23e97322bfe42d6ccfc9b4
Alex Z <can4you@yandex.ru>
2021-11-21 19:40:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace OTUS_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstDirectory = new DirectoryInfo(@"c:\Otus\TestDir1");
+            var secondDirectory = new DirectoryInfo(@"c:\Otus\TestDir2");
+            firstDirectory.Create();
+            secondDirectory.Create();
+
+            CreateFiles(firstDirectory.ToString());
+            CreateFiles(secondDirectory.ToString());
+
+            WriteToFile(firstDirectory.ToString(), "File");
+            WriteToFile(secondDirectory.ToString(), "File");
+
+            WriteToFile(firstDirectory.ToString(), DateTime.Now);
+            WriteToFile(secondDirectory.ToString(), DateTime.Now);
+
+            ReadFromFile(firstDirectory.ToString());
+            ReadFromFile(secondDirectory.ToString());
+
+            Console.ReadLine();
+        }
+
+        static void CreateFiles(string path)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                string auxiliaryString = path + @"\File" + i + ".txt";
+                var myFile = File.Create(auxiliaryString);
+                myFile.Close();
+            }
+        }
+
+        static void WriteToFile(string directory, string message)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                string auxiliaryString = directory + @"\File" + i + ".txt";
+
+                if (File.Exists(auxiliaryString))
+                {
+                    using (StreamWriter writer = new StreamWriter(auxiliaryString, true))
+                    {
+                        writer.Write(message + i + "\t");
+                        writer.Close();
+                    }
+                }
+            }
+        }
+
+        static void WriteToFile(string directory, DateTime dateTime)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                string auxiliaryString = directory + @"\File" + i + ".txt";
+
+                if (File.Exists(auxiliaryString))
+                {
+                    using (StreamWriter writer = new StreamWriter(auxiliaryString, true))
+                    {
+                        writer.Write(dateTime.ToString());
+                        writer.Close();
+                    }
+                }
+            }
+        }
+
+        static void ReadFromFile(string directory)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                string auxiliaryString = directory + @"\File" + i + ".txt";
+
+                if (File.Exists(auxiliaryString))
+                {
+                    using (StreamReader reader = new StreamReader(auxiliaryString))
+                    {
+                        string str = reader.ReadToEnd();
+                        string[] arrayStr = str.Split('\t');
+                        Console.WriteLine($"имя_файла: File" + i + $" текст: {arrayStr[0]} " + $"дополнение: {arrayStr[1]}");
+                        reader.Close();
+                    }
+                }
+            }
+        }
+    }
+}
